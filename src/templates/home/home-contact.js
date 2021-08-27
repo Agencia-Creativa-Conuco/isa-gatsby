64a209e25c4c28e5c538ab8afaec1b100e69ac84
Osirs Peña <osiris.pena@conuco.do>
2021-08-27 00:11:09 -0400

    form - prueba con netlify

--- src/templates/home/home-contact.js
+++ src/templates/home/home-contact.js
@@ -34,7 +34,7 @@ const HomeContact = ({ page }) =>{
                     <Col size={12} sizeLG={6}>
                         <Content bgDeco={colors.primary.light}>
                             <SectionTitle> {title} </SectionTitle>
-                            <Form name="contact" method="POST" data-netlify="true">
+                            <Form name="contacto" method="POST" netlify>
                                 <Input type="text" name="name" placeholder="Nombre"/>
                                 <Input type="email" name="email" placeholder="Correo Electronico"/>
                                 <Input type="tel" name="phone" placeholder="Numero de telefono"/>
