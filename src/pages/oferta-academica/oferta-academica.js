dfe49a39479b34eb819dae79455d00465c219f54
Osirs Peña <osiris.pena@conuco.do>
2021-10-12 09:35:29 -0400

    oferta academica - actualizando estructura

--- src/pages/oferta-academica/oferta-academica.js
+++ src/pages/oferta-academica/oferta-academica.js
@@ -4,6 +4,7 @@ import OfferCover from "./offer-cover";
 import OfferFaculties from "./offer-faculties";
 import OfferTecnics from "./offer-tecnics";
 import OfferInternational from "./offer-international";
+import OfferLabs from "./offer-labs";
 import Layout from "../../components/layout";
 
 const OfferPage = (props) => {
@@ -15,6 +16,7 @@ const OfferPage = (props) => {
         <OfferCover />
         <OfferFaculties />
         <OfferTecnics />
+        <OfferLabs />
         <OfferInternational />
       </Container>
     </Layout>
