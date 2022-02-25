92663ac67a2ab1c3c3d16f5547280c3016ce8728
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-02-25 15:01:06 -0400

    Nostros -Update component  admisiones-info

--- src/templates/page/admisiones/admisiones-cover.js
+++ src/templates/page/admisiones/admisiones-cover.js
@@ -1,16 +1,10 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import {
-
-  Row,
-  Col,
-} from '../../../components/layout/index'
-import FeaturedMedia from '../../../components/featured-media'
-import colors from '../../../components/styles/colors'
-import Cta from '../../../components/cta'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from "react";
+import styled from "@emotion/styled";
+import FeaturedMedia from "../../../components/featured-media";
+import colors from "../../../components/styles/colors";
+import Cta from "../../../components/cta";
+import { useStaticQuery, graphql } from "gatsby";
 import { container, mq } from "../../../components/layout/new/";
-
 
 const AdmisionesCover = () => {
   //Obtiene las imágenes localmente desde la ruta "images/home"
@@ -28,7 +22,7 @@ const AdmisionesCover = () => {
         }
       }
     }
-  `)
+  `);
 
   // Convierte arreglo de imágenes en objeto cuya llave es el nómbre del archivo
   // Esto para facilitar la búsqueda de la imagenes en los componentes hijos.
@@ -36,88 +30,79 @@ const AdmisionesCover = () => {
     return {
       ...obj,
       [item.name]: item,
-    }
-  }, {})
+    };
+  }, {});
 
-  const title = 'Estudia con nosotros',
+  const title = "Estudia con nosotros",
     copy =
-      'Sigue estos pasos y estudia con nosotros, aplica para vivir una experiencia educativa de calidad que marcará un antes y un después en tu carrera profesional. ¿Estás listo?',
+      "Sigue estos pasos y estudia con nosotros, aplica para vivir una experiencia educativa de calidad que marcará un antes y un después en tu carrera profesional. ¿Estás listo?",
     cta = {
-      url: '#form',
-      title: 'Aplicar',
-    }
+      url: "#form",
+      title: "Aplicar",
+    };
   return (
     <Section spaceNone fluid>
-        {/* <Row alignCenter>
-          <Col size={12} sizeMD={7} sizeLG={6} noGutters> */}
-            <DivLogo decoBg={colors.secondary.light}>
-              <DecoLogo decoBg={colors.secondary.light}>
-                <Logo
-                  media={images.cover}
-                  alt="Admisiones Universidad ISA"
-                  size="100%"
-                  sizeXL="90%"
-                  bgColor
-                  position="35% 50%"
-                  loading="eager"
-                />
-              </DecoLogo>
-            </DivLogo>
-          {/* </Col>
-          <Col size={12} sizeMD={5} sizeLG={6}> */}
-            <Content as="div" decoBg={colors.secondary.light}>
-              <SectionTitle>{title}</SectionTitle>
-              <Copy>{copy}</Copy>
-              <StyledCTA to={cta.url} paddingX="7rem">
-                {cta.title}
-              </StyledCTA>
-            </Content>
-          {/* </Col>
-        </Row> */}
+      <DivLogo decoBg={colors.secondary.light}>
+        <DecoLogo decoBg={colors.secondary.light}>
+          <Logo
+            media={images.cover}
+            alt="Admisiones Universidad ISA"
+            size="100%"
+            sizeXL="90%"
+            bgColor
+            position="35% 50%"
+            loading="eager"
+          />
+        </DecoLogo>
+      </DivLogo>
+      <Content as="div" decoBg={colors.secondary.light}>
+        <SectionTitle>{title}</SectionTitle>
+        <Copy>{copy}</Copy>
+        <StyledCTA to={cta.url} paddingX="7rem">
+          {cta.title}
+        </StyledCTA>
+      </Content>
     </Section>
-  )
-}
+  );
+};
 
-export default AdmisionesCover
+export default AdmisionesCover;
 
-const Section = styled.section `
-${container}
-padding: 0;
+const Section = styled.section`
+  ${container}
+  padding: 0;
 
-display:grid;
-grid-template-columns: 100%;
+  display: grid;
+  grid-template-columns: 100%;
 
-${mq.md}{
-  grid-template-columns: 50% 50%;
-
-}
-
+  ${mq.md} {
+    grid-template-columns: 50% 50%;
+  }
 `;
 
 const Content = styled.div`
-  align-self:center;
+  align-self: center;
   position: relative;
   max-width: 58rem;
-  padding: 7% 1.5rem ;
+  padding: 7% 1.5rem;
   margin-left: auto;
   margin-right: auto;
   z-index: 1;
 
-  ${mq.md}{
-
-  &:after {
-    content: '';
-    position: absolute;
-    top: 0%;
-    right: 0%;
-    transform: translate(90%, -40%);
-    background-color: ${(props) => props.decoBg};
-    padding: 5%;
-    opacity: 0.7;
-    z-index: -1;
+  ${mq.md} {
+    &:after {
+      content: "";
+      position: absolute;
+      top: 0%;
+      right: 0%;
+      transform: translate(90%, -40%);
+      background-color: ${(props) => props.decoBg};
+      padding: 5%;
+      opacity: 0.7;
+      z-index: -1;
+    }
   }
-}
-`
+`;
 
 const SectionTitle = styled.h1`
   margin-top: 0;
@@ -127,20 +112,20 @@ const SectionTitle = styled.h1`
   ${mq.md} {
     text-align: left;
   }
-`
+`;
 
-const Copy = styled.p``
+const Copy = styled.p``;
 
 const StyledCTA = styled(Cta)`
   ${mq.md} {
     margin-top: 3rem;
   }
-`
+`;
 
 const DivLogo = styled.div`
   position: relative;
   &::before {
-    content: '';
+    content: "";
     background-color: ${(props) => props.decoBg};
     position: absolute;
     padding: 15%;
@@ -151,7 +136,7 @@ const DivLogo = styled.div`
   }
 
   &::after {
-    content: '';
+    content: "";
     background-color: ${(props) => props.decoBg};
     position: absolute;
     padding: 20%;
@@ -160,12 +145,12 @@ const DivLogo = styled.div`
     transform: translate(2rem, 0rem);
     opacity: 0.2;
   }
-`
+`;
 
 const DecoLogo = styled.div`
   position: relative;
   &:before {
-    content: '';
+    content: "";
     position: absolute;
     padding: 5%;
     right: 13%;
@@ -174,7 +159,7 @@ const DecoLogo = styled.div`
     opacity: 0.7;
   }
   &&:after {
-    content: '';
+    content: "";
     position: absolute;
     padding: 1.7%;
     right: 6%;
@@ -182,9 +167,9 @@ const DecoLogo = styled.div`
     background-color: ${(props) => props.decoBg};
     opacity: 0.7;
   }
-`
+`;
 
 const Logo = styled(FeaturedMedia)`
   clip-path: ellipse(97% 100% at left 88%);
   z-index: 5;
-`
+`;
--- src/templates/page/admisiones/admisiones-info.js
+++ src/templates/page/admisiones/admisiones-info.js
@@ -1,251 +1,251 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
-import { Container, Section, Row, Col, mq} from "../../../components/layout/index";
-import {AdmissionRequisiteIcon} from "../../../components/icons";
-import {h2} from "../../../components/styles/tipography";
-import {Spring, animated} from "@react-spring/web";
+import { AdmissionRequisiteIcon } from "../../../components/icons";
+import { h2 } from "../../../components/styles/tipography";
+import { Spring, animated } from "@react-spring/web";
 import colors from "../../../components/styles/colors";
-import useGruposDeRequisitos from '../../../hooks/useGruposDeRequisitos';
-import useGrados from '../../../hooks/useGrados';
+import useGruposDeRequisitos from "../../../hooks/useGruposDeRequisitos";
+import useGrados from "../../../hooks/useGrados";
+import { container, mq } from "../../../components/layout/new/";
 
-const AdmisionesInfo = () =>{
+const AdmisionesInfo = () => {
+  const categories = useGruposDeRequisitos();
 
-    const categories = useGruposDeRequisitos();
-
-    //Obtiene los grados 
-    const grades = useGrados()?.sort((a,b) => a.order - b.order).filter( grade => {
-        return grade.requisitos.length;
+  //Obtiene los grados
+  const grades = useGrados()
+    ?.sort((a, b) => a.order - b.order)
+    .filter((grade) => {
+      return grade.requisitos.length;
     });
 
-    const [view, setView] = useState(0);
+  const [view, setView] = useState(0);
 
-    return grades.length ?(
-        <Section spaceNone id="section_2">
-            <Container fluid>
-                <Row>
+  return grades.length ? (
+    <Section fluid spaceNone id="section_2">
+      <Navigation
+        css={css`
+          background-color: ${colors.secondary.light};
+        `}
+      >
+        <ColumnContainer>
+          <Icon>
+            <AdmissionRequisiteIcon />
+          </Icon>
+          <ImtemUl>
+            {grades.map((grade, index) => {
+              const isActive = view === index;
 
-                    <Col 
-                        size={12} 
-                        sizeMD={4} 
-                        sizeLG={4} 
-                        order={1}
-                        orderMD={2}
-                        noGutterns 
-                        css={css`background-color:${colors.secondary.light};`}
-                        >
-                        <Navigation>
-                            <Row alignSMItems="flex-end">
-                                <Col size={12} sizeSM={6} sizeMD={12} orderMD={2}>
-                                    <Icon color={colors.secondary.dark}>
-                                        <AdmissionRequisiteIcon/>
-                                    </Icon>
-                                </Col>
-                                <Col>
-                                    <ImtemUl>
-                                    {
-                                        grades.map((grade,index)=>{
+              const { nombre, id } = grade;
 
-                                            const isActive = view === index;
+              return (
+                <Item
+                  key={id}
+                  color={isActive ? "white" : colors.secondary.dark}
+                  onClick={() => setView(index)}
+                  {...{ isActive }}
+                >
+                  <ItemName>{nombre}</ItemName>
+                </Item>
+              );
+            })}
+          </ImtemUl>
+        </ColumnContainer>
+      </Navigation>
 
-                                            const {
-                                                nombre,
-                                                id
-                                            } = grade;
+      <Displayer
+        css={css`
+          background-color: ${colors.secondary.lighter};
+        `}
+      >
+        {grades.map((grade, index) => {
+          const { requisitos = [] } = grade;
 
-                                            return (
-                                                <Item 
-                                                    key={id}
-                                                    color={isActive?"white":colors.secondary.dark}
-                                                    onClick={() => setView(index)}
-                                                    {...{isActive}}
-                                                >
-                                                    <ItemName>{nombre}</ItemName>
-                                                </Item>
-                                            )
-                                        })
-                                    }
-                                    </ImtemUl>
-                                </Col>
-                            </Row>
-                        </Navigation>
-                    </Col>
+          const isActive = view === index;
 
-                    <Col 
-                     size={12}
-                     sizeMD={7}
-                     sizeLG={7}
-                     order={2}
-                     orderMD={1}
-                     noGutterns 
-                     css={css`background-color:${colors.secondary.lighter};`}>
-                         <Displayer>
-                            {
-                                grades.map((grade,index)=>{
+          return (
+            <Spring
+              key={index}
+              reset={isActive}
+              from={{ opacity: 0 }}
+              to={[{ opacity: 1 }]}
+            >
+              {(styles) => (
+                <animated.div style={styles}>
+                  <DisplayerSection key={index} hidden={view !== index}>
+                    <DisplayerSectionTitle color={colors.secondary.dark}>
+                      Requisitos de admisión {grade.title}
+                    </DisplayerSectionTitle>
 
-                                    const {
-                                        requisitos = [],
-                                    } = grade;
+                    <Groups>
+                      {categories.map((group, index) => {
+                        const { id, name } = group;
 
-                                    const isActive = view === index;
+                        const catRequirements = requisitos.filter((item) => {
+                          return item.categoria
+                            .map((category) => category.id)
+                            .includes(id);
+                        });
 
-                                    return (
-                                    <Spring
-                                        key={index} 
-                                        reset={ isActive}
-                                        from={{ opacity: 0}}
-                                        to={[
-                                        { opacity: 1},
-                                        ]}
-                                    >
-                                    {
-                                        styles =>(
-                                            <animated.div style={styles}>
-                                                <DisplayerSection key={index} hidden={view !== index}>
-                                                    <DisplayerSectionTitle color={colors.secondary.dark}>Requisitos de admisión {grade.title}</DisplayerSectionTitle>
-                                                    
-                                                    <Groups>
-                                                    {
-                                                        categories.map((group,index)=>{
-
-                                                            const {
-                                                                id, 
-                                                                name
-                                                            } = group;
-
-                                                            const catRequirements = requisitos.filter( item => {
-                                                                return item.categoria.map( category => category.id ).includes(id);
-                                                            })
-                                                            
-                                                            return catRequirements.length? (
-                                                                <Group key={id} color={colors.secondary.dark}>
-                                                                    <GroupName>{name}</GroupName>
-                                                                    <Requirements>
-                                                                    {
-                                                                        catRequirements.map((item, index)=>{
-                                                                            const {requisito} = item;
-                                                                            return (
-                                                                                <Requirement key={index} color={colors.secondary.dark}>{requisito}</Requirement>
-                                                                            );
-                                                                        })
-                                                                    }
-                                                                    </Requirements>
-                                                                </Group>
-                                                            ): null
-                                                        })
-                                                    }
-                                                    </Groups>
-                                                    
-                                                </DisplayerSection>
-                                            </animated.div>
-                                        )
-                                    }
-                                    </Spring>
-                                    )
-                                })
-                            }
-                         </Displayer>
-                    </Col>
-
-                </Row>
-            </Container>
-        </Section>
-    ):null;
-
-}
+                        return catRequirements.length ? (
+                          <Group key={id} color={colors.secondary.dark}>
+                            <GroupName>{name}</GroupName>
+                            <Requirements>
+                              {catRequirements.map((item, index) => {
+                                const { requisito } = item;
+                                return (
+                                  <Requirement
+                                    key={index}
+                                    color={colors.secondary.dark}
+                                  >
+                                    {requisito}
+                                  </Requirement>
+                                );
+                              })}
+                            </Requirements>
+                          </Group>
+                        ) : null;
+                      })}
+                    </Groups>
+                  </DisplayerSection>
+                </animated.div>
+              )}
+            </Spring>
+          );
+        })}
+      </Displayer>
+    </Section>
+  ) : null;
+};
 
 export default AdmisionesInfo;
 
-const Navigation = styled.div`
-    margin: 0 auto;
-    padding: 10%;
-    ${mq.sm}{
-        padding: 5%;
-    }
-    ${mq.md}{
-        padding: 3rem;
-    }
+const Section = styled.section`
+  ${container}
+  padding: 0;
+
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-areas:
+    "col_2"
+    "col_1";
+
+  ${mq.md} {
+    grid-template-columns: 58% 33% 9%;
+    grid-template-areas: "col_1 col_2 space";
+  }
 `;
+const Navigation = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 10%;
+  grid-area: col_2;
+  ${mq.sm} {
+    padding: 5%;
+  }
+  ${mq.md} {
+    padding: 3rem;
+  }
+`;
+
+const ColumnContainer = styled.div`
+  display: grid;
+  grid-template-columns: 100%;
+  ${mq.sm} {
+      grid-template-columns: 1fr 1fr;
+}
+${mq.md} {
+  grid-template-columns: 100%;
+
+}
+`;
+
 const ImtemUl = styled.ul`
-    margin:0;
-    padding: 0;
+  margin: 0;
+  padding: 0;
+  align-self: end;
 `;
 
 const Item = styled.li`
-    ${({color="green", isActive})=>css`
-        ${h2}
-        cursor: pointer;
-        font-weight: bold;
-        text-align: right;
-        text-transform: uppercase;
-        list-style: none;
-        color: ${color};
-        margin: 0;
-        margin-bottom: 0rem;
-        padding: .5rem 0;
-        transition: all 0.25s ease-in-out;
-        ${isActive?css`
-            transform-origin: 100% 0%;
-            transform: scale(1.2);
-        `:css``}
-    `}
+  ${({ color = "green", isActive }) => css`
+    ${h2}
+    cursor: pointer;
+    font-weight: bold;
+    text-align: right;
+    text-transform: uppercase;
+    list-style: none;
+    color: ${color};
+    margin: 0;
+    margin-bottom: 0rem;
+    padding: 0.5rem 0;
+    transition: all 0.25s ease-in-out;
+    ${isActive
+      ? css`
+          transform-origin: 100% 0%;
+          transform: scale(1.2);
+        `
+      : css``}
+  `}
 `;
 
 const ItemName = styled.span`
-    display: block;
+  display: block;
 `;
 
-const Displayer = styled.div``;
+const Displayer = styled.div`
+  grid-area: col_1;
+`;
 
 const DisplayerSection = styled.div`
-    margin: 0;
-    text-align: right;
-    padding: 10%;
-    ${mq.sm}{
-        padding: 5%;
-    }
-    ${mq.md}{
-        padding: 3rem;
-    }
+  margin: 0;
+  text-align: right;
+  padding: 10%;
+  ${mq.sm} {
+    padding: 5%;
+  }
+  ${mq.md} {
+    padding: 3rem;
+  }
 `;
 
 const DisplayerSectionTitle = styled.h2`
-    ${({color="red"})=>css`
-        color: ${color};
-        text-transform: uppercase;
-        font-weight: 900;
-        margin-top: 0;
-    `}
+  ${({ color = "red" }) => css`
+    color: ${color};
+    text-transform: uppercase;
+    font-weight: 900;
+    margin-top: 0;
+  `}
 `;
 
 const Groups = styled.ul``;
 
 const Group = styled.li`
-    ${({color="green"})=>css`
-        color: ${color};
-        list-style: none;
-    `}
+  ${({ color = "green" }) => css`
+    color: ${color};
+    list-style: none;
+  `}
 `;
 
 const GroupName = styled.h3`
-    text-transform: uppercase;
-    font-weight: 600;
-    color: inherit;
+  text-transform: uppercase;
+  font-weight: 600;
+  color: inherit;
 `;
 
 const Requirements = styled.ul``;
 
 const Requirement = styled.li`
-    ${({color="green"})=>css`
-        color: ${color};
-        list-style: none;
-    `}
+  color: ${colors.secondary.dark};
+  list-style: none;
 `;
 
 const Icon = styled.div`
-    ${({color="green"})=>css`
-        color: ${color};
-        max-width: 30rem;
-        margin: 2rem auto;
-    `}
+  color: ${colors.secondary.dark};
+  max-width: 30rem;
+  margin: 2rem auto;
+  order: initial;
+
+  ${mq.md} {
+    order: 2;
+  }
 `;
