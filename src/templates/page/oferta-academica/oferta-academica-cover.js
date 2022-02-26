c3bcdd92f46bb1a41550ae3cc995923e960ae6e8
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-02-26 14:36:43 -0400

    Oferta Academica -Update component  oferta-academica-cover

--- src/templates/page/oferta-academica/oferta-academica-cover.js
+++ src/templates/page/oferta-academica/oferta-academica-cover.js
@@ -1,15 +1,9 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import {
-  Container,
-  Section,
-  Row,
-  Col,
-  mq,
-} from '../../../components/layout/index'
-import FeaturedMedia from '../../../components/featured-media'
-import colors from '../../../components/styles/colors'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from "react";
+import styled from "@emotion/styled";
+import FeaturedMedia from "../../../components/featured-media";
+import colors from "../../../components/styles/colors";
+import { useStaticQuery, graphql } from "gatsby";
+import { container, mq } from "../../../components/layout/new/";
 
 const OfferCover = () => {
   //Obtiene las imágenes localmente desde la ruta "images/home"
@@ -27,7 +21,7 @@ const OfferCover = () => {
         }
       }
     }
-  `)
+  `);
 
   // Convierte arreglo de imágenes en objeto cuya llave es el nómbre del archivo
   // Esto para facilitar la búsqueda de la imagenes en los componentes hijos.
@@ -35,67 +29,77 @@ const OfferCover = () => {
     return {
       ...obj,
       [item.name]: item,
-    }
-  }, {})
+    };
+  }, {});
 
-  const title = 'Oferta Académica',
+  const title = "Oferta Académica",
     copy =
-      'La Universidad ha sabido responder a los cambios que la sociedad ha demandado, y en la actualidad ha diversificado su oferta curricular ofreciendo carreras que responden a las necesidades del país. En todas ellas introduciendo cambios en la estructura curricular para formar profesionales con las competencias requeridas para desarrollar su labor con profesionalidad, inspirados en los principios y valores que sustenta la Universidad.'
+      "La Universidad ha sabido responder a los cambios que la sociedad ha demandado, y en la actualidad ha diversificado su oferta curricular ofreciendo carreras que responden a las necesidades del país. En todas ellas introduciendo cambios en la estructura curricular para formar profesionales con las competencias requeridas para desarrollar su labor con profesionalidad, inspirados en los principios y valores que sustenta la Universidad.";
 
   return (
-    <Section spaceNone>
-      <Container fluid>
-        <Row alignCenter>
-          <Col size={12} sizeMD={6} sizeLG={7} noLGutters>
-            <DivLogo decoBg={colors.blue.dark} decoBgA={colors.blue.base}>
-              <Image
-                media={images.cover}
-                alt={title}
-                size="100%"
-                sizeMD="120%"
-                sizeLG="100%"
-                sizeXL="80%"
-                position="90% 50%"
-                bgColor
-                loading="eager"
-              />
-            </DivLogo>
-          </Col>
-          <Col size={12} sizeMD={6} sizeLG={5}>
-            <DivTitle>
-              <SectionTitle> {title} </SectionTitle>
-              <div dangerouslySetInnerHTML={{ __html: copy }} />
-            </DivTitle>
-          </Col>
-        </Row>
-      </Container>
+    <Section spaceNone fluid>
+      <DivLogo>
+        <Image
+          media={images.cover}
+          alt={title}
+          size="100%"
+          sizeMD="124%"
+          sizeLG="100%"
+          sizeXL="80%"
+          position="90% 50%"
+          bgColor
+          loading="eager"
+        />
+      </DivLogo>
+      <DivTitle>
+        <SectionTitle> {title} </SectionTitle>
+        <div dangerouslySetInnerHTML={{ __html: copy }} />
+      </DivTitle>
     </Section>
-  )
-}
+  );
+};
 
-export default OfferCover
+export default OfferCover;
+
+const Section = styled.section`
+  ${container}
+  padding: 0;
+
+  display: grid;
+  grid-template-columns: 100%;
+  ${mq.md} {
+    grid-template-columns: 50% 50%;
+  }
+  ${mq.md} {
+    grid-template-columns: 55% 45%;
+  }
+`;
 
 const DivLogo = styled.div`
   position: relative;
+
+padding-right:1.5rem ;
   &::before {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
-    background-color: ${(props) => props.decoBgA};
+    background-color: ${colors.blue.base};
     padding: 4%;
     clip-path: circle(50% at 50% 50%);
     right: 0;
     opacity: 0.8;
     transform: translate(-2rem, 7rem);
+    z-index: -1;
+
     ${mq.md} {
       transform: translate(${8.2 * 100}%, 9rem);
     }
   }
   &::after {
-    content: '';
+    content: "";
     position: absolute;
     bottom: 0;
-    background-color: ${(props) => props.decoBg};
+    background-color: ${colors.blue.dark};
     padding: 20%;
     clip-path: circle(50% at 50% 100%);
     right: -3%;
@@ -105,17 +109,23 @@ const DivLogo = styled.div`
       z-index: 0;
     }
   }
-`
+`;
 
 const DivTitle = styled.div`
-  max-width: 57rem;
+  max-width: 60rem;
+  align-self: center;
+  padding: 0 1.5rem;
   margin: 10% auto;
   ${mq.md} {
     margin: 10rem auto;
   }
-`
+`;
 
-const SectionTitle = styled.h1``
+const SectionTitle = styled.h1`\
+margin-top: 0;
+
+
+`;
 
 const Image = styled(FeaturedMedia)`
   clip-path: ellipse(88% 75% at left);
@@ -123,4 +133,4 @@ const Image = styled(FeaturedMedia)`
   ${mq.md} {
     clip-path: ellipse(100% 100% at left);
   }
-`
+`;
