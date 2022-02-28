b0c2befec5f44758f4661d6a1b4e0c99b998fda7
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-02-28 14:33:48 -0400

    DIP - Update layout del componente dip-cover

--- src/templates/page/dip/dip-cover.js
+++ src/templates/page/dip/dip-cover.js
@@ -1,60 +1,48 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
-import { Container, Section, Row, Col } from '../../../components/layout/index'
-import FeaturedMedia from '../../../components/featured-media'
-import colors from '../../../components/styles/colors'
-import useFiles from '../../../hooks/useFiles'
+import React from "react";
+import styled from "@emotion/styled";
+import FeaturedMedia from "../../../components/featured-media";
+import colors from "../../../components/styles/colors";
+import useFiles from "../../../hooks/useFiles";
+import { container, mq } from "../../../components/layout/new/";
 
 const DIPCover = () => {
-  const images = useFiles()
+  const images = useFiles();
 
-  const title = 'Investigación',
+  const title = "Investigación",
     copy =
-      'La Vicerrectoría de Investigaciones es la unidad operativa de la Universidad ISA que tiene como misión institucional normar, dirigir y desarrollar las actividades de investigación de la institución.',
-    featuredImage = images.investigacion.cover
+      "La Vicerrectoría de Investigaciones es la unidad operativa de la Universidad ISA que tiene como misión institucional normar, dirigir y desarrollar las actividades de investigación de la institución.",
+    featuredImage = images.investigacion.cover;
 
   return (
-    <StyledSection spaceNone decoBg={colors.cta.base}>
-      <Container fluid>
-        <Row alignCenter>
-          <Col size={12} sizeMD={5} sizeLG={6} order={2} orderMD={1}>
-            <Content as="div">
-              <Title> {title} </Title>
-              <Copy>{copy}</Copy>
-            </Content>
-          </Col>
-          <Col
-            size={11}
-            sizeSM={10}
-            sizeMD={7}
-            sizeLG={6}
-            order={1}
-            orderMD={2}
-            mlAuto
-            noGutters
-          >
-            <DecoLogo decoBg={colors.blue.dark}>
-              <Logo
-                media={featuredImage}
-                size="100%"
-                sizeMD="100%"
-                sizeXL="90%"
-                bgColor
-                loading="eager"
-              />
-            </DecoLogo>
-          </Col>
-        </Row>
-      </Container>
-    </StyledSection>
-  )
-}
+    <Section fluid>
+      <Content as="div">
+        <Title> {title} </Title>
+        <Copy>{copy}</Copy>
+      </Content>
+      <DecoLogo decoBg={colors.blue.dark}>
+        <Logo
+          media={featuredImage}
+          size="100%"
+          sizeMD="90%"
+          sizeXL="90%"
+          bgColor
+          loading="eager"
+        />
+      </DecoLogo>
+    </Section>
+  );
+};
 
-export default DIPCover
+export default DIPCover;
 
-const StyledSection = styled(Section)`
-  ${({ decoBg = 'blue' }) => css`
+const Section = styled.section`
+  ${container}
+  padding: 0;
+
+  display: grid;
+  grid-template-columns: 100%;
+  
+
     &:before {
       content: '';
       position: absolute;
@@ -63,32 +51,50 @@ const StyledSection = styled(Section)`
       width: 30%;
       padding-bottom: 30%;
       border-radius: 50%;
-      background: ${decoBg};
+      background: ${colors.cta.base};
       transform: translate(-90%, 0);
     }
-  `}
-`
+    div:first-of-type {
+      order: 2;
+    }
+    ${mq.md} {
+    grid-template-columns: 45% 55%;
 
-const Content = styled(Section)`
-  max-width: 57rem;
-  margin-left: auto;
-  margin-right: auto;
-`
+    div:last-of-type {
+      order: 2;
+    }
+    ${mq.lg} {
+    grid-template-columns: 50% 50%;
+  }
+
+`;
+
+const Content = styled.div`
+  max-width: 60rem;
+  align-self: center;
+  padding: 0 1.5rem;
+  margin: 10% auto;
+  z-index: 2;
+`;
 
 const Title = styled.h1`
   margin-top: 0;
-`
+`;
 
-const Copy = styled.p``
+const Copy = styled.p``;
 
 const Logo = styled(FeaturedMedia)`
   clip-path: ellipse(100% 100% at right);
-`
+`;
 
 const DecoLogo = styled.div`
+  margin-left: 9%;
+  ${mq.md} {
+    margin-left: 0;
+  }
   position: relative;
   &::after {
-    content: '';
+    content: "";
     position: absolute;
     left: -8%;
     bottom: 15%;
@@ -96,4 +102,4 @@ const DecoLogo = styled.div`
     clip-path: circle(50% at 50% 50%);
     background: ${(props) => props.decoBg};
   }
-`
+`;
