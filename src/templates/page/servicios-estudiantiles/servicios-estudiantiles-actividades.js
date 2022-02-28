ba1cff1e18269d1b591734e3768ef2cd6ee4a9e4
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-02-28 10:09:23 -0400

    Decanto Estudiantes - Update layout del componente sevicios-estudiantes-actividades

--- src/templates/page/servicios-estudiantiles/servicios-estudiantiles-actividades.js
+++ src/templates/page/servicios-estudiantiles/servicios-estudiantiles-actividades.js
@@ -1,55 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import styled from '@emotion/styled'
-import {
-  Container,
-  Row,
-  Col,
-  Section,
-  mq,
-} from '../../../components/layout/index'
-import Carousel from 'react-slick'
-import FeaturedMedia from '../../../components/featured-media'
-import { h2 } from '../../../components/styles/tipography'
-import colors from '../../../components/styles/colors'
-import Cta from '../../../components/cta'
-import useFiles from '../../../hooks/useFiles'
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Carousel from "react-slick";
+import FeaturedMedia from "../../../components/featured-media";
+import { h2 } from "../../../components/styles/tipography";
+import colors from "../../../components/styles/colors";
+import Cta from "../../../components/cta";
+import useFiles from "../../../hooks/useFiles";
+import { container, mq } from "../../../components/layout/new/";
 
 const ServiciosEstudiantilesActividades = () => {
-  const images = useFiles()
+  const images = useFiles();
 
-  const title = 'Grupos y clubes estudiantiles',
+  const title = "Grupos y clubes estudiantiles",
     actividadesExtracurriculares = [
       {
-        title: 'CAISA',
-        copy:
-          'El Club Ambiental del ISA (CAISA) es un voluntariado que promueve la conservación, preservación y estudio del medio ambiente; conformado por estudiantes, profesores y personal de apoyo de la Institución, interesados en realizar actividades en fomento y conservación de los recursos naturales en la República Dominicana.',
+        title: "CAISA",
+        copy: "El Club Ambiental del ISA (CAISA) es un voluntariado que promueve la conservación, preservación y estudio del medio ambiente; conformado por estudiantes, profesores y personal de apoyo de la Institución, interesados en realizar actividades en fomento y conservación de los recursos naturales en la República Dominicana.",
         cta: null,
-        image: images['servicios-estudiantiles'].caisa,
-        position: '50% 50%',
+        image: images["servicios-estudiantiles"].caisa,
+        position: "50% 50%",
       },
       {
-        title: 'Club de Astronomía',
-        copy:
-          'Es un club integrado por estudiantes de múltiples disciplinas con el interés en común por conocer y divulgar los conocimientos sobre astronomía. En el club se proporciona un ambiente y los materiales adecuados para desarrollar las aptitudes relacionadas con las ciencias espaciales.',
+        title: "Club de Astronomía",
+        copy: "Es un club integrado por estudiantes de múltiples disciplinas con el interés en común por conocer y divulgar los conocimientos sobre astronomía. En el club se proporciona un ambiente y los materiales adecuados para desarrollar las aptitudes relacionadas con las ciencias espaciales.",
         cta: null,
-        image: images['servicios-estudiantiles'].astronomia,
-        position: '50% 70%',
+        image: images["servicios-estudiantiles"].astronomia,
+        position: "50% 70%",
       },
       {
-        title: 'Club de Lectura',
-        copy:
-          'Es un espacio donde los miembros tienen la oportunidad de reunirse con personas que comparten su pasión por la literatura, con el fin de fortalecer a la comunidad lectora para explotar el máximo potencial de cada uno de los integrantes.',
+        title: "Club de Lectura",
+        copy: "Es un espacio donde los miembros tienen la oportunidad de reunirse con personas que comparten su pasión por la literatura, con el fin de fortalecer a la comunidad lectora para explotar el máximo potencial de cada uno de los integrantes.",
         cta: null,
-        image: images['servicios-estudiantiles'].lectura,
-        position: '50% 50%',
+        image: images["servicios-estudiantiles"].lectura,
+        position: "50% 50%",
       },
       {
-        title: 'Pastoral Juvenil',
-        copy:
-          'Desarrolla acciones encaminadas a fomentar valores, permitiendo a los jóvenes descubrir habilidades de crecimiento personal, fomentar la solidaridad y el respeto, comprometiéndose con Jesucristo y su mensaje.',
+        title: "Pastoral Juvenil",
+        copy: "Desarrolla acciones encaminadas a fomentar valores, permitiendo a los jóvenes descubrir habilidades de crecimiento personal, fomentar la solidaridad y el respeto, comprometiéndose con Jesucristo y su mensaje.",
         cta: null,
-        image: images['servicios-estudiantiles'].pastoral_juvenil,
-        position: '50% 40%',
+        image: images["servicios-estudiantiles"].pastoral_juvenil,
+        position: "50% 40%",
       },
       // {
       //   title: 'Club De Danza',
@@ -60,12 +50,11 @@ const ServiciosEstudiantilesActividades = () => {
       //   position: '50% 30%',
       // },
       {
-        title: 'Club De Ciencias',
-        copy:
-          'Tiene como misión el contribuir con el fortalecimiento de la enseñanza- aprendizaje de las ciencias en la República Dominicana a través del desarrollo intelectual, científico, cultural y social de los profesores de ciencias de la naturaleza en el país.',
+        title: "Club De Ciencias",
+        copy: "Tiene como misión el contribuir con el fortalecimiento de la enseñanza- aprendizaje de las ciencias en la República Dominicana a través del desarrollo intelectual, científico, cultural y social de los profesores de ciencias de la naturaleza en el país.",
         cta: null,
-        image: images['servicios-estudiantiles'].club_de_ciencias,
-        position: '50% 30%',
+        image: images["servicios-estudiantiles"].club_de_ciencias,
+        position: "50% 30%",
       },
       // {
       //   title: 'Cine Foro',
@@ -76,42 +65,34 @@ const ServiciosEstudiantilesActividades = () => {
       //   position: '50% 50%',
       // },
       {
-        title: 'Club de fotografía',
-        copy:
-          'Es un grupo formado por estudiantes apasionados por la fotografía, con el objetivo de aprender más acerca del mundo de este fantástico arte y poder desarrollar sus habilidades sin importar que equipo tenga.',
+        title: "Club de fotografía",
+        copy: "Es un grupo formado por estudiantes apasionados por la fotografía, con el objetivo de aprender más acerca del mundo de este fantástico arte y poder desarrollar sus habilidades sin importar que equipo tenga.",
         cta: null,
-        image: images['servicios-estudiantiles'].cine_foro,
-        position: '50% 50%',
+        image: images["servicios-estudiantiles"].cine_foro,
+        position: "50% 50%",
       },
       {
-        title: 'CINECLUB',
-        copy:
-          'Pretende vincular experiencias compartidas a través de un diálogo con el cine, descubrir, observar y debatir de forma crítica la representación cinematográfica. Además de ofrecer un espacio dinámico y creativo que más allá del rol del espectador pasivo, potencie una reflexión colectiva entre compañeros.',
+        title: "CINECLUB",
+        copy: "Pretende vincular experiencias compartidas a través de un diálogo con el cine, descubrir, observar y debatir de forma crítica la representación cinematográfica. Además de ofrecer un espacio dinámico y creativo que más allá del rol del espectador pasivo, potencie una reflexión colectiva entre compañeros.",
         cta: null,
-        image: images['servicios-estudiantiles'].cine_foro,
-        position: '50% 50%',
+        image: images["servicios-estudiantiles"].cine_foro,
+        position: "50% 50%",
       },
-    ]
+    ];
 
-  const [nav1, setNav1] = useState(null)
-  const [nav2, setNav2] = useState(null)
-  const [slider1, setSlider1] = useState([])
-  const [slider2, setSlider2] = useState([])
+  const [nav1, setNav1] = useState(null);
+  const [nav2, setNav2] = useState(null);
+  const [slider1, setSlider1] = useState([]);
+  const [slider2, setSlider2] = useState([]);
 
   useEffect(() => {
-    setNav1(slider1)
-    setNav2(slider2)
-  }, [slider1, slider2])
+    setNav1(slider1);
+    setNav2(slider2);
+  }, [slider1, slider2]);
 
   return actividadesExtracurriculares ? (
-    <Section spaceBottomNone id="section_1">
-      <Container>
-        <Row>
-          <Col>
-            <SectionTitle>{title}</SectionTitle>
-          </Col>
-        </Row>
-      </Container>
+    <Section spaceBottomNone id="section_1" fluid>
+      <SectionTitle>{title}</SectionTitle>
       <InfoContainer>
         <ImageWrapper>
           <ImageCarousel
@@ -123,7 +104,7 @@ const ServiciosEstudiantilesActividades = () => {
             ref={(slider) => setSlider1(slider)}
           >
             {actividadesExtracurriculares.map((item, index) => {
-              const { image, position } = item
+              const { image, position } = item;
 
               return (
                 <FeaturedMedia
@@ -134,7 +115,7 @@ const ServiciosEstudiantilesActividades = () => {
                   position={position}
                   bgColor
                 />
-              )
+              );
             })}
           </ImageCarousel>
         </ImageWrapper>
@@ -146,7 +127,7 @@ const ServiciosEstudiantilesActividades = () => {
             appendDots={(dots) => (
               <Dots>
                 {dots.map((item, index) => {
-                  return <Dot key={index}>{item}</Dot>
+                  return <Dot key={index}>{item}</Dot>;
                 })}
               </Dots>
             )}
@@ -154,54 +135,63 @@ const ServiciosEstudiantilesActividades = () => {
             ref={(slider) => setSlider2(slider)}
           >
             {actividadesExtracurriculares.map((item, index) => {
-              const { title, copy, cta } = item
+              const { title, copy, cta } = item;
 
               return (
                 <Slide key={index}>
-                  <Container noGutters>
-                    <Row>
-                      <Col>
-                        <Content>
-                          <Title color={colors.shadow.base}>{title}</Title>
-                          <Copy color={colors.shadow.text}>{copy}</Copy>
-                          {cta ? (
-                            <Cta to={cta.url} target={cta.target}>
-                              {cta.title}
-                            </Cta>
-                          ) : null}
-                        </Content>
-                      </Col>
-                    </Row>
-                  </Container>
+                  <Content>
+                    <Title>{title}</Title>
+                    <Copy>{copy}</Copy>
+                    {cta ? (
+                      <Cta to={cta.url} target={cta.target}>
+                        {cta.title}
+                      </Cta>
+                    ) : null}
+                  </Content>
                 </Slide>
-              )
+              );
             })}
           </ContentCarousel>
         </ContentWrapper>
       </InfoContainer>
     </Section>
-  ) : null
-}
+  ) : null;
+};
 
-export default ServiciosEstudiantilesActividades
+export default ServiciosEstudiantilesActividades;
+
+const Section = styled.section`
+  ${container}
+  padding: 0;
+  margin-top: 5.5rem;
+  ${mq.md} {
+    margin-top: 9.6rem;
+  }
+
+`;
+
+const Container = styled.div`
+  ${container}
+  padding: 0;
+`;
 
 const SectionTitle = styled.h2`
   text-align: center;
   margin-bottom: 4rem;
-`
+`;
 
 const InfoContainer = styled.div`
   overflow: hidden;
   position: relative;
   font-size: 0;
-`
+`;
 
 const ContentWrapper = styled.div`
   position: relative;
   z-index: 2;
   padding: 5rem 1.5rem;
   &:before {
-    content: '';
+    content: "";
     background-color: ${colors.primary.base};
     position: absolute;
     top: 0;
@@ -225,23 +215,23 @@ const ContentWrapper = styled.div`
     right: 5%;
     max-width: 57rem;
   }
-`
+`;
 
-const ImageWrapper = styled.div``
+const ImageWrapper = styled.div``;
 
-const ImageCarousel = styled(Carousel)``
+const ImageCarousel = styled(Carousel)``;
 
 const ContentCarousel = styled(Carousel)`
   height: 100%;
   padding-bottom: 5rem;
-`
+`;
 
 const Dots = styled.ul`
   margin: 0;
   padding: 0;
   position: absolute;
   bottom: 2rem;
-`
+`;
 
 const Dot = styled.div`
   margin: 0;
@@ -268,27 +258,31 @@ const Dot = styled.div`
       opacity: 1;
     }
   }
-`
+`;
 
-const Slide = styled.div``
+const Slide = styled.div``;
 
 const Content = styled.div`
   font-size: 1.6rem;
   ${mq.md} {
+    padding: 0 2.5rem;
     text-align: right;
   }
-`
+  ${mq.lg} {
+    padding: 0;
+  }
+`;
 
 const Title = styled.h3`
   color: white;
   text-transform: uppercase;
-  text-shadow: ${(props) => props.color};
+  text-shadow: ${colors.shadow.base};
   margin-bottom: 4rem;
   font-weight: 900;
   ${h2}
-`
+`;
 
 const Copy = styled.p`
   color: white;
-  text-shadow: ${(props) => props.color};
-`
+  text-shadow: ${colors.shadow.text};
+`;
