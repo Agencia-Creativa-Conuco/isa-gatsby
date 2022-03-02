a97da7637ab9a1e7ea2101af06789a9cecfbee29
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-03-02 09:22:15 -0400

    DIP - Update component dip-cover

--- src/templates/page/dep/dep-cover.js
+++ src/templates/page/dep/dep-cover.js
@@ -1,15 +1,9 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import {
-  Container,
-  Section,
-  Row,
-  Col,
-  mq,
-} from '../../../components/layout/index'
-import FeaturedMedia from '../../../components/featured-media'
-import colors from '../../../components/styles/colors'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from "react";
+import styled from "@emotion/styled";
+import FeaturedMedia from "../../../components/featured-media";
+import colors from "../../../components/styles/colors";
+import { useStaticQuery, graphql } from "gatsby";
+import { container, mq } from "../../../components/layout/new";
 
 const DEPCover = () => {
   //Obtiene las imágenes localmente desde la ruta "images/home"
@@ -31,7 +25,7 @@ const DEPCover = () => {
         }
       }
     }
-  `)
+  `);
 
   // Convierte arreglo de imágenes en objeto cuya llave es el nómbre del archivo
   // Esto para facilitar la búsqueda de la imagenes en los componentes hijos.
@@ -39,101 +33,107 @@ const DEPCover = () => {
     return {
       ...obj,
       [item.name]: item,
-    }
-  }, {})
+    };
+  }, {});
 
-  const title = 'Dirección Extensión y Proyectos',
+  const title = "Dirección Extensión y Proyectos",
     copy =
-      'La Dirección de Extensión y Proyectos es una dependencia de la Vicerrectoría de Investigación, Extensión y Postgrado (VIEP), la misma ha sido como un instrumento para la difusión del conocimiento, la aplicación de la ciencia, el apoyo a los sectores productivos y empresariales, el mejoramiento de la competitividad, así como el intercambio cultural y deportivo, todo ello visto como fundamento para el desarrollo integral de la sociedad y el cumplimiento de la labor de la Universidad.'
+      "La Dirección de Extensión y Proyectos es una dependencia de la Vicerrectoría de Investigación, Extensión y Postgrado (VIEP), la misma ha sido como un instrumento para la difusión del conocimiento, la aplicación de la ciencia, el apoyo a los sectores productivos y empresariales, el mejoramiento de la competitividad, así como el intercambio cultural y deportivo, todo ello visto como fundamento para el desarrollo integral de la sociedad y el cumplimiento de la labor de la Universidad.";
 
   return (
-    <Section spaceNone>
-      <Container fluid noGutters>
-        <Row alignCenter>
-          <Col size={12} sizeLG={7}>
-            <DecoLogo decoBg={colors.blue.base}>
-              <Logo
-                media={images.cover}
-                size="80%"
-                position="100% 50%"
-                alt="Dirección Extensión y Proyectos"
-                bgColor
-                loading="eager"
-              />
-              <DecoLogo2 decoBg={colors.blue.dark} />
-            </DecoLogo>
-          </Col>
-          <Col>
-            <Col>
-              <Content>
-                <DivTitle>
-                  <SectionTitle> {title}</SectionTitle>
-                  <Copy>{copy}</Copy>
-                </DivTitle>
-              </Content>
-            </Col>
-          </Col>
-        </Row>
-      </Container>
-    </Section>
-  )
-}
+    <Section fluid spaceNone>
+      <DecoLogo>
+        <Logo
+          media={images.cover}
+          size="80%"
+          position="100% 50%"
+          alt="Dirección Extensión y Proyectos"
+          bgColor
+          loading="eager"
+        />
+        <DecoLogo2 />
+      </DecoLogo>
 
-export default DEPCover
+      <Content>
+        <DivTitle>
+          <SectionTitle> {title}</SectionTitle>
+          <Copy>{copy}</Copy>
+        </DivTitle>
+      </Content>
+    </Section>
+  );
+};
+
+export default DEPCover;
+
+const Section = styled.section`
+  ${container}
+  padding: 0;
+  display: grid;
+  grid-template-columns: 100%;
+
+  ${mq.lg} {
+    grid-template-columns: 60% 40%;
+  }
+`;
 
 const DivTitle = styled.div`
-  ${mq.xl} {
+  max-width: 60rem;
+
+  ${mq.lg} {
     max-width: 57rem;
     margin: 0 auto;
   }
-`
+`;
 
 const SectionTitle = styled.h1`
   margin-top: 0;
   margin-bottom: 3rem;
-`
+`;
 
-const Copy = styled.p``
+const Copy = styled.p``;
 
 const Logo = styled(FeaturedMedia)`
   clip-path: circle(85% at 20% 3%);
-`
+`;
 const Content = styled.div`
-  margin: 2rem auto;
+  margin: 4rem auto 2rem auto;
+  padding: 0 1.5rem;
   max-width: 50rem;
   position: relative;
-`
+  align-self: center;
+`;
 const DecoLogo = styled.div`
   position: relative;
   &::before {
-    content: '';
+    content: "";
     position: absolute;
     right: 23%;
     bottom: 0;
-    background: ${(props) => props.decoBg};
+    background: ${colors.blue.base};
     width: 17%;
     padding-bottom: 35%;
     opacity: 0.4;
   }
   &::after {
-    content: '';
+    content: "";
     position: absolute;
     right: 7%;
     bottom: 0;
-    background: ${(props) => props.decoBg};
+    background: ${colors.blue.base};
     width: 16%;
     padding-bottom: 12%;
   }
-`
+`;
 
 const DecoLogo2 = styled.div`
   &::before {
-    content: '';
+    content: "";
     position: absolute;
     right: 10%;
     bottom: 21%;
-    background: ${(props) => props.decoBg};
+    background: ${colors.blue.dark};
     padding: 4.5%;
     clip-path: circle(50% at 50% 50%);
   }
-`
+`;
