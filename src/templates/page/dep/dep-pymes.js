07c8ff37897432ba82e7864724de6c9abad4aa3c
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-03-02 11:13:29 -0400

    DEP - Update component dip-pymes

--- src/templates/page/dep/dep-pymes.js
+++ src/templates/page/dep/dep-pymes.js
@@ -1,65 +1,50 @@
 import React from "react";
 import styled from "@emotion/styled";
 import Cta from "../../../components/cta";
-import {
-  Container,
-  Section,
-  Row,
-  Col,
-  mq,
-} from "../../../components/layout/index";
 import FeaturedMedia from "../../../components/featured-media";
 import colors from "../../../components/styles/colors";
 import useFiles from "../../../hooks/useFiles";
+import { container, mq } from "../../../components/layout/new";
 
 const DEPPymes = () => {
-
   const images = useFiles();
   const image = images["direccion-extension-y-proyectos"].centro_mipymes;
 
-  const 
-    title = "Centro MIPYMES", 
-    copy = "El Centro MIPYMES (Centro de Servicios de Apoyo Integral de las micro, pequeñas y medianas empresas) es una iniciativa por parte del Ministerio de Industria y Comercio a través del Viceministerio de Fomento a las  MIPYMES en alianza con la Universidad ISA, el cual tiene como objetivo principal brindar los servicios y las consultorías necesarias a las micro, pequeñas y medianas empresas, de manera gratuita, para fortalecer su capacidad productiva, competitividad y eficiencia.", 
-    cta ={
+  const title = "Centro MIPYMES",
+    copy =
+      "El Centro MIPYMES (Centro de Servicios de Apoyo Integral de las micro, pequeñas y medianas empresas) es una iniciativa por parte del Ministerio de Industria y Comercio a través del Viceministerio de Fomento a las  MIPYMES en alianza con la Universidad ISA, el cual tiene como objetivo principal brindar los servicios y las consultorías necesarias a las micro, pequeñas y medianas empresas, de manera gratuita, para fortalecer su capacidad productiva, competitividad y eficiencia.",
+    cta = {
       url: "/centro-mipymes",
       title: "Conoce más",
-      target: null
+      target: null,
     };
-  
+
   return (
     <Section spaceBottomNone id="section_4">
-      <WrapperRow bg={colors.blue.base} decoBg={colors.blue.dark}>
-        <Container>
-          <Row>
-            <Col size={12} sizeMD={5} noGutters>
-              <Media decoBg={colors.blue.dark}>
-                <FeaturedMedia media={image} size="100%" mxAuto />
-              </Media>
-            </Col>
-            <Col size={12} sizeMD={6} mxAuto>
-              <Content>
-                <SectionTitle>{title}</SectionTitle>
-                <DivTitle color={colors.white} decoBg={colors.blue.dark}>
-                  <Copy>{copy}</Copy>
-                </DivTitle>
-                {cta ? (
-                  <Cta to={cta.url} target={cta.target}>
-                    {cta.title}
-                  </Cta>
-                ) : null}
-              </Content>
-            </Col>
-          </Row>
-        </Container>
-      </WrapperRow>
+      <Container>
+        <Media>
+          <FeaturedMedia media={image} size="100%" mxAuto />
+        </Media>
+        <Content>
+          <SectionTitle>{title}</SectionTitle>
+          <DivTitle>
+            <Copy>{copy}</Copy>
+          </DivTitle>
+          {cta ? (
+            <Cta to={cta.url} target={cta.target}>
+              {cta.title}
+            </Cta>
+          ) : null}
+        </Content>
+      </Container>
     </Section>
   );
 };
 
 export default DEPPymes;
 
-const WrapperRow = styled.div`
-  background: ${(props) => props.bg};
+const Section = styled.section`
+  background: ${colors.blue.base};
   margin-top: 15rem;
   position: relative;
   &:before {
@@ -69,17 +54,27 @@ const WrapperRow = styled.div`
     padding-bottom: 17%;
     right: 0;
     top: 0;
-    background: ${(props) => props.decoBg};
+    background: ${colors.blue.dark};
     opacity: 0.8;
   }
 `;
 
+const Container = styled.div`
+  ${container}
+  padding: 0;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-column-gap: 7rem;
+  ${mq.md} {
+    grid-template-columns: 42% 50%;
+  }
+`;
 const Content = styled.div`
-  padding: 0 0 4rem 0;
+  padding: 0 1.5rem 4rem 1.5rem;
 `;
 
 const DivTitle = styled.div`
-  color: ${(props) => props.color};
+  color: ${colors.white};
   padding: 5rem 0;
   padding-top: 0;
   ${mq.md} {
@@ -118,6 +113,6 @@ const Media = styled.div`
     padding-bottom: 10%;
     right: 25%;
     top: 13%;
-    background: ${(props) => props.decoBg};
+    background: ${colors.blue.dark};
   }
 `;
