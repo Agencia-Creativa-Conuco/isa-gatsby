0017ec0744c6d2cfda998c58788708678ab46f9e
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-03-03 17:13:26 -0400

    Carrera - Update carrera-pensum y carrera-form

--- src/templates/carrera/carrera-form.js
+++ src/templates/carrera/carrera-form.js
@@ -1,74 +1,73 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
-import { Container, Section, Row, Col, mq } from '../../components/layout/index'
-import colors from '../../components/styles/colors'
-import BackgroundImage from 'gatsby-background-image'
-import Form from '../../components/form'
-import useGrados from '../../hooks/useGrados'
-import Cta from '../../components/cta'
+import React from "react";
+import styled from "@emotion/styled";
+import colors from "../../components/styles/colors";
+import BackgroundImage from "gatsby-background-image";
+import Form from "../../components/form";
+import useGrados from "../../hooks/useGrados";
+import Cta from "../../components/cta";
+import { container, mq } from "../../components/layout/new/";
 
 const CarreraForm = ({ carrera, facultad }) => {
-  const { imagenFormulario } = carrera
+  const { imagenFormulario } = carrera;
 
-  const [grado] = useGrados().filter((grado) => grado.id === carrera.grado.id)
+  const [grado] = useGrados().filter((grado) => grado.id === carrera.grado.id);
 
   const formIds = grado.formularios.hsFormularios.map(
-    (formulario) => formulario.idFormulario,
-  )
+    (formulario) => formulario.idFormulario
+  );
 
-  const googleForm = grado.formularios.googleFormulario
+  const googleForm = grado.formularios.googleFormulario;
 
   return grado.formularios.tipo ? (
-    <BGSection spaceNone>
+    <Section>
       <BackgroundImage
         fluid={imagenFormulario?.localFile?.childImageSharp?.fluid}
       >
         <Container>
-          <Row>
-            <Col
-              size="auto"
-              css={css`
-                background-color: ${colors.gray.light};
-              `}
-            >
-              <Wrapper>
-                <Title>Solicitud de admisión</Title>
-                {grado.formularios.tipo === 'hubspot' ? (
-                  <Form formIds={formIds} cardStyle={false} />
-                ) : grado.formularios.tipo === 'google' ? (
-                  <Cta to={googleForm} target="_blank">
-                    Solicitar
-                  </Cta>
-                ) : null}
-              </Wrapper>
-            </Col>
-          </Row>
+          <Wrapper>
+            <Title>Solicitud de admisión</Title>
+            {grado.formularios.tipo === "hubspot" ? (
+              <Form formIds={formIds} cardStyle={false} />
+            ) : grado.formularios.tipo === "google" ? (
+              <Cta to={googleForm} target="_blank">
+                Solicitar
+              </Cta>
+            ) : null}
+          </Wrapper>
         </Container>
       </BackgroundImage>
-    </BGSection>
-  ) : null
-}
+    </Section>
+  ) : null;
+};
 
-export default CarreraForm
+export default CarreraForm;
 
-const BGSection = styled(Section)`
-  background: url(${(props) => props.bg});
+const Section = styled.section`
+  /* background: url(${(props) => props.bg}); */
   background-size: cover;
   background-repeat: no-repeat;
   background-position: 50% 50%;
-`
+`;
+
+const Container = styled.div`
+  ${container}
+  padding: 0;
+  display: grid;
+  grid-template-columns: 100%;
+  justify-content: flex-start;
+`;
 
 const Wrapper = styled.div`
   max-width: 40rem;
   min-height: 50rem;
   padding: 2rem 0;
+  background-color: ${colors.gray.light};
   ${mq.md} {
     padding: 4rem;
   }
-`
+`;
 
 const Title = styled.h2`
   text-transform: uppercase;
   margin-bottom: 3rem;
-`
+`;
--- src/templates/carrera/carrera-pensum.js
+++ src/templates/carrera/carrera-pensum.js
@@ -1,122 +1,113 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
-import {Section, Container, Row, Col, mq} from "../../components/layout/index";
-import {PensumIcon} from "../../components/icons";
+import { PensumIcon } from "../../components/icons";
 import useRecursos from "../../hooks/useRecursos";
+import { container, mq } from "../../components/layout/new/";
 
-const CarreraPensum = ({ carrera, facultad })=>{
+const CarreraPensum = ({ carrera, facultad }) => {
+  const { recursos = [] } = carrera;
 
-    const {
-        recursos = []
-    } = carrera;
+  const pensums = useRecursos().filter((recurso) => {
+    const { id, tipoRecurso } = recurso;
 
-    
-    const pensums = useRecursos().filter((recurso)=>{
-        
-        const {
-            id,
-            tipoRecurso
-        } = recurso;
+    //verifica que el recurso esta enlazado a la carrera
+    const esRecurso = recursos.filter((item) => item.id === id).length;
 
-        //verifica que el recurso esta enlazado a la carrera
-        const esRecurso = recursos.filter( (item) => item.id === id ).length
+    return tipoRecurso === "pensum" && esRecurso;
+  });
 
-        return tipoRecurso === "pensum" && esRecurso;
-    });
+  const facultyColor = facultad.color;
 
-    const facultyColor = facultad.color;
+  return pensums.length ? (
+    <Section>
+      <Container>
+        {pensums.map((pensum, index) => {
+          const { nombre, archivo } = pensum;
 
-    return pensums.length?(
-        <Section css={sectionStyles}>
-            <Container>
-                <Row>
-                {
-                    pensums.map((pensum, index)=>{
-                        const{
-                            nombre,
-                            archivo
-                        } = pensum;
-
-                        return(
-                            <Col size={12} sizeSM="auto" mxAuto key={index}>
-                                <Card>
-                                    <CardLink
-                                        href={archivo}
-                                        download
-                                    >
-                                        <Row alignCenter>
-                                            <Col>
-                                                <CardSpan color={facultyColor}>Descargar</CardSpan>
-                                                <CardTitle color={facultyColor}>{ nombre }</CardTitle>
-                                            </Col>
-                                            <Col size="auto">
-                                                <CardIcon color={facultyColor}>
-                                                    <PensumIcon />
-                                                </CardIcon>
-                                            </Col>
-                                        </Row>
-                                    </CardLink>
-                                </Card>
-                            </Col>
-                        )
-                    })   
-                }
-                </Row>
-            </Container>
-        </Section>
-    ):null;
-}
+          return (
+            <Card key={index}>
+              <CardLink href={archivo} download>
+                <CardContainer>
+                  <CardSpan color={facultyColor}>Descargar</CardSpan>
+                  <CardTitle color={facultyColor}>{nombre}</CardTitle>
+                </CardContainer>
+                <CardIcon color={facultyColor}>
+                  <PensumIcon />
+                </CardIcon>
+              </CardLink>
+            </Card>
+          );
+        })}
+      </Container>
+    </Section>
+  ) : null;
+};
 
 export default CarreraPensum;
 
-const sectionStyles = css`
-    margin-top: -10rem;
-    ${mq.md}{
-        margin-top: -15rem;
-    }
+const Section = styled.div`
+  margin-top: -10rem;
+  margin-bottom: 5.5rem;
+
+  ${mq.md} {
+    margin-top: -15rem;
+    margin-bottom: 9.6rem;
+  }
+`;
+
+const Container = styled.div`
+  ${container}
+  padding: 0;
+
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const Card = styled.div`
-    box-shadow: .5rem .5rem .5rem rgba(0,0,0,0.15);
-    background-color: white;
-    border-radius: 1.5rem;
-    max-width: 32rem;
-    margin: 0 auto;
-    margin-bottom: 2rem;
-    position: relative;
-    z-index: 2;
+  box-shadow: 0.5rem 0.5rem 0.5rem rgba(0, 0, 0, 0.15);
+  background-color: white;
+  border-radius: 1.5rem;
+  max-width: 32rem;
+  margin: 0 auto;
+  margin-bottom: 2rem;
+  position: relative;
+  z-index: 2;
 `;
 
 const CardLink = styled.a`
-    padding: 1.5rem;
-    display: block;
-    text-decoration: none;
-    cursor: pointer;
-    color: inherit;
+  padding: 1.5rem;
+  display: block;
+  text-decoration: none;
+  cursor: pointer;
+  color: inherit;
+  display: flex;
 `;
 
+const CardContainer = styled.div`
+  padding: 0 1.5rem;
+`;
 const CardTitle = styled.h3`
-    ${({color="green"})=>css`
-        color: ${color};
-        text-transform: uppercase;
-        font-weight: 900;
-        margin: 0;
-        font-size: 2rem;
-    `}
+  ${({ color = "green" }) => css`
+    color: ${color};
+    text-transform: uppercase;
+    font-weight: 900;
+    margin: 0;
+    font-size: 2rem;
+  `}
 `;
 
 const CardSpan = styled.span`
-    ${({color="green"})=>css`
-        color: ${color};
-        text-transform: uppercase;
-        font-weight: 300;
-    `}
+  ${({ color = "green" }) => css`
+    color: ${color};
+    text-transform: uppercase;
+    font-weight: 300;
+  `}
 `;
 
 const CardIcon = styled.div`
-    ${({color="green"})=>css`
-        max-width: 8rem;
-        color: ${color};
-    `}
+  ${({ color = "green" }) => css`
+    max-width: 8rem;
+    color: ${color};
+  `}
 `;
