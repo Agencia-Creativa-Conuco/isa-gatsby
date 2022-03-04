33ddc2b81e82a36bcbbdaad19072b1ecfc201a11
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-03-04 12:15:26 -0400

    Search - Update layout de los componentes resourceslist y card

--- src/components/card.js
+++ src/components/card.js
@@ -1,49 +1,50 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Row, Col, Container } from "./layout/index";
 import FeaturedMedia from "./featured-media";
 import Cta from "./cta";
+import { container, mq } from "./layout/new";
 
 const CardInfo = ({ item = {} }) => {
-  const {
-    nombre,
-    imagenPortada,
-    descripcion,
-    archivo,
-  } = item;
+  const { nombre, imagenPortada, descripcion, archivo } = item;
 
   return (
-    <Wrapper>
-      <Container>
-        <Row>
-          <Col order={2} orderLG={1}>
-            <Content>
-              <Title>{nombre}</Title>
-              <Copy>{descripcion}</Copy>
-              <Cta to={archivo} download>
-                Descargar
-              </Cta>
-            </Content>
-          </Col>
-          {imagenPortada ? (
-            <Col size={12} sizeLG={6} order={1} orderLG={2}>
-              <Media>
-                <FeaturedMedia media={imagenPortada} bgColor="rgba( 0 0 0 / 10%)" />
-              </Media>
-            </Col>
-          ) : null}
-        </Row>
-      </Container>
+    <Wrapper value={imagenPortada ? "50% auto" : "100%"}>
+      <Content>
+        <Title>{nombre}</Title>
+        <Copy>{descripcion}</Copy>
+        <Cta to={archivo} download>
+          Descargar
+        </Cta>
+      </Content>
+      {imagenPortada ? (
+        <Media>
+          <FeaturedMedia media={imagenPortada} bgColor="rgba( 0 0 0 / 10%)" />
+        </Media>
+      ) : null}
     </Wrapper>
   );
 };
 
 export default CardInfo;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  ${container};
+  display: grid;
+
+  grid-template-columns: 100%;
+  div:first-of-type {
+    order: 2;
+  }
+  ${mq.lg} {
+    grid-template-columns: ${(props) => props.value};
+    div:last-of-type {
+      order: 2;
+    }
+  }
+`;
 
 const Content = styled.div`
-    margin: 2rem 0;
+padding:0 1.5rem ;
 `;
 
 const Title = styled.h2`
@@ -56,7 +57,10 @@ const Copy = styled.p`
 `;
 
 const Media = styled.div`
-    margin: 2rem 0;
-    max-width: 30rem;
-    box-shadow: .2rem .2rem .2rem rgba(0 0 0 / .5);
+  width: 100%;
+  align-self: center;
+  margin: 2rem 0;
+  height: fit-content;
+  max-width: 30rem;
+  box-shadow: 0.2rem 0.2rem 0.2rem rgba(0 0 0 / 0.5);
 `;
--- src/components/resourceslist.js
+++ src/components/resourceslist.js
@@ -1,178 +1,200 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import {Container, Row, Col, Section} from "./layout/index";
-import {h3} from "./styles/tipography";
-import {SearchIcon} from "./icons";
+import { h3 } from "./styles/tipography";
+import { SearchIcon } from "./icons";
 import ResourceCard from "./resource-card";
 import colors from "./styles/colors";
 import useRecursos from "../hooks/useRecursos";
 import useModal from "./hooks/useModal";
 import ctas from "./styles/cta";
+import { container, mq } from "./layout/new";
 
 const ResourcesList = ({
-    spaceTopNone, 
-    spaceBottomNone, 
-    spaceNone, 
-    title= "Recursos", 
-    titleAs= "h2", 
-    titleColor= colors.primary.dark,
-    resourceColor= colors.primary.dark,
-    searchEnable = false,
-    alwaysVisibile = false,
-    noResultsText = "Sin resultados para mostrar",
-    thin = false,
-    small = false,
-    medium = true,
-    large = false,
-    exclude = [],
-    items = []
+  spaceTopNone,
+  spaceBottomNone,
+  spaceNone,
+  title = "Recursos",
+  titleAs = "h2",
+  titleColor = colors.primary.dark,
+  resourceColor = colors.primary.dark,
+  searchEnable = false,
+  alwaysVisibile = false,
+  noResultsText = "Sin resultados para mostrar",
+  thin = false,
+  small = false,
+  medium = true,
+  large = false,
+  exclude = [],
+  items = [],
 }) => {
+  const { openModal, ModalUI } = useModal();
 
-    const {
-        openModal, 
-        ModalUI
-    } = useModal();
+  const resources = useRecursos().filter((recurso) => {
+    const { id, tipoRecurso } = recurso;
 
-    const resources = useRecursos().filter((recurso)=>{
+    //Verifica que el recurso está relacionado al post.
+    const esRelacionado = items.filter((item) => item.id === id).length;
 
-        const {
-            id,
-            tipoRecurso
-        } = recurso;
+    return !exclude.includes(tipoRecurso) && esRelacionado;
+  });
 
-        //Verifica que el recurso está relacionado al post.
-        const esRelacionado = items.filter( item => item.id === id ).length;
+  useEffect(() => {}, []);
 
-        return !exclude.includes(tipoRecurso) && esRelacionado;
-    });
+  return resources.length || alwaysVisibile ? (
+    <Section
+      {...{
+        spaceTopNone,
+        spaceBottomNone,
+        spaceNone,
+        thin,
+        small,
+        medium,
+        large,
+      }}
+    >
+      <ContainerUI>
+        <Title as={titleAs} color={titleColor}>
+          {title}
+        </Title>
+        <Button onClick={openModal}>Ver recursos</Button>
+      </ContainerUI>
 
-    useEffect(()=>{
-    }, []);
+      <ModalUI>
+        <Container>
+          <Title as={titleAs} color={titleColor}>
+            {title}
+          </Title>
 
-    return ( resources.length || alwaysVisibile)?(
-        <Section {...{spaceTopNone, spaceBottomNone, spaceNone, thin, small, medium, large}}>
-            <Container>
-                    <Row>
-                        <Col size="auto" mxAuto>
-                            <Title as={titleAs} color={titleColor}>{title}</Title>
-                            <Button onClick={openModal}>Ver recursos</Button>
-                        </Col>
-                    </Row>
-            </Container>
-            <ModalUI>
-               <Container>
-                    <Row>
-                        <Col>
-                            <Title as={titleAs} color={titleColor}>{title}</Title>
-                        </Col>
-                    </Row>
-                    {
-                        searchEnable?(
-                        <Row>
-                            <Col>
-                                <SearchForm role="search" aria-label="Buscar:" onSubmit={(e)=>{}}>
-                                    <SearchInput type="text" placeholder="Buscar:" onInput={(e)=>{}} />
-                                    <SearchButton colors={colors} ><SearchIcon /></SearchButton>
-                                </SearchForm>
-                            </Col>
-                        </Row>
-                        ):null
-                    }
-                    <Row justifyContent="center">
-                    {
-                        resources.length? resources.map((item,index) =>{
+          {searchEnable ? (
+            <SearchForm role="search" aria-label="Buscar:" onSubmit={(e) => {}}>
+              <SearchInput
+                type="text"
+                placeholder="Buscar:"
+                onInput={(e) => {}}
+              />
+              <SearchButton colors={colors}>
+                <SearchIcon />
+              </SearchButton>
+            </SearchForm>
+          ) : null}
+          <ContainerCard>
+            {resources.length ? (
+              resources.map((item, index) => {
+                const { nombre, imagenPortada, archivo } = item;
 
-                            const {
-                                nombre,
-                                imagenPortada,
-                                archivo
-                            } = item;
-
-                            return(
-                            <Col key={index} size="auto">
-                                <ResourceCard
-                                    title={nombre}
-                                    icon={imagenPortada}
-                                    to={archivo}    
-                                    color={resourceColor}
-                                    item={item}
-                                />
-                            </Col>
-                            )
-                        }):(
-                            <Col>
-                                <Message color={colors.red.base}>{noResultsText}</Message>
-                            </Col>
-                        )
-                    }
-                    </Row>
-                </Container>
-            </ModalUI>
-        </Section>
-    ): null;
-}
+                return (
+                  <ResourceCard
+                    key={index}
+                    title={nombre}
+                    icon={imagenPortada}
+                    to={archivo}
+                    color={resourceColor}
+                    item={item}
+                  />
+                );
+              })
+            ) : (
+              <Message color={colors.red.base}>{noResultsText}</Message>
+            )}
+          </ContainerCard>
+        </Container>
+      </ModalUI>
+    </Section>
+  ) : null;
+};
 
 export default ResourcesList;
 
+const Section = styled.section`
+  margin-bottom: 6.4rem;
+  margin-top: 6.4rem;
+  ${mq.md} {
+    margin-bottom: 12.8rem;
+    margin-top: 12.8rem;
+  }
+`;
+const Container = styled.div`
+  ${container}
+`;
+
+const ContainerUI = styled.div`
+  ${container}
+  text-align:center;
+`;
+
+const ContainerCard = styled.div`
+  ${container}
+  display:grid;
+  grid-template-columns: 100%;
+  grid-column-gap: 2rem;
+  justify-content: center;
+  ${mq.lg} {
+    grid-template-columns: 40% 40%;
+  }
+  ${mq.xl} {
+    grid-template-columns: 33.3333% 33.3333% 33.3333%;
+  }
+`;
+
 const Title = styled.h2`
-    ${({color}) => css`
-        text-align: center;
-        font-weight: 900;
-        margin-bottom: 50px;
-        color: ${color};
-    `}
+  ${({ color }) => css`
+    text-align: center;
+    font-weight: 900;
+    margin-bottom: 50px;
+    color: ${color};
+  `}
 `;
 
 const SearchForm = styled.form`
-    max-width: 600px;
-    margin: 0 auto;
-    margin-bottom: 80px;
-    position: relative;
+  max-width: 600px;
+  margin: 0 auto;
+  margin-bottom: 80px;
+  position: relative;
 `;
 
 const SearchInput = styled.input`
-    width: 100%;
-    padding-left: 20px;
-    padding-right: 80px;
-    outline: none;
-    border: none;
-    height: 50px;
-    background-color: rgba(0,0,0,0.1);
-    border-radius: 50px;
-    font-size: 20px;
+  width: 100%;
+  padding-left: 20px;
+  padding-right: 80px;
+  outline: none;
+  border: none;
+  height: 50px;
+  background-color: rgba(0, 0, 0, 0.1);
+  border-radius: 50px;
+  font-size: 20px;
 `;
 
 const SearchButton = styled.button`
-    ${({colors})=>css`
-        width: 60px;
-        height: 60px;
-        border-radius: 50%;
-        color: white;
-        outline: none;
-        border: none;
-        position: absolute;
-        top: 50%;
-        right: 0;
-        transform: translate(0, -50%);
-        background-color: ${colors.primary.base};
-        cursor: pointer;
-        text-align: center;
-        svg{
-            display: inline-block;
-            transform: scale(1.2);
-        }
-    `}
+  ${({ colors }) => css`
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    color: white;
+    outline: none;
+    border: none;
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translate(0, -50%);
+    background-color: ${colors.primary.base};
+    cursor: pointer;
+    text-align: center;
+    svg {
+      display: inline-block;
+      transform: scale(1.2);
+    }
+  `}
 `;
 
 const Message = styled.p`
-    ${h3}
-    ${({color})=>css`
-        text-align: center;
-        color: ${color};
-    `}
+  ${h3}
+  ${({ color }) => css`
+    text-align: center;
+    color: ${color};
+  `}
 `;
 
 const Button = styled.button`
-    ${ctas}
+  ${ctas}
 `;
