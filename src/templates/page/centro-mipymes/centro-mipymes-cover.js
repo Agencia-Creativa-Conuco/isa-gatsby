014285c6a57004f1227d2e5f0004ef600644e977
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-04-29 10:57:19 -0400

    CentroMipymes-agregando cover

--- src/templates/page/centro-mipymes/centro-mipymes-cover.js
+++ src/templates/page/centro-mipymes/centro-mipymes-cover.js
@@ -1,9 +1,102 @@
 import React from 'react'
 
+import styled from "@emotion/styled";
+import FeaturedMedia from "../../../components/featured-media";
+import colors from "../../../components/styles/colors";
+import Cta from "../../../components/cta";
+import { useStaticQuery, graphql } from "gatsby";
+import { container, mq } from "../../../components/layout/new/";
+
 const CentroMipymesCover = () => {
+
+
+
+    const { allFile } = useStaticQuery(graphql`
+    query {
+      allFile(filter: { relativeDirectory: { in: ["centro-mipymes"] } }) {
+        nodes {
+          id
+          name
+          childImageSharp {
+            fluid(maxWidth: 1200) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const images = allFile.nodes.reduce((obj, item) => {
+    return {
+      ...obj,
+      [item.name]: item,
+    };
+  }, {});
+
+const data = {
+  title: "CENTRO MIPYMES",
+  copy: "El Centro MIPYMES (Centro de Servicios de Apoyo Integral de las micro, pequeñas y medianas empresas) es una iniciativa por parte del Ministerio de Industria y Comercio a través del Viceministerio de Fomento a las MIPYMES en alianza con la Universidad ISA, el cual tiene como objetivo principal brindar los servicios y las consultorías necesarias a las micro, pequeñas y medianas empresas, de manera gratuita, para fortalecer su capacidad productiva, competitividad y eficienci",
+  logo: images.logo,
+};
+
+
   return (
-    <div>CentroMipymesCover</div>
+    <Section>
+      <Logo>
+      
+          <FeaturedMedia
+            media={data.logo}
+            alt="CENTRO MIPYMES"
+            loading="eager"
+            />
+            </Logo>
+          <SectionTitle>{data.title}</SectionTitle>
+
+        <Copy>{data.copy}</Copy>
+    </Section>
   )
 }
 
 export default CentroMipymesCover
+
+const Section = styled.section`
+  ${container}
+  /* padding: 0; */
+
+  display: grid;
+  grid-template-columns: 100%;
+  justify-items: center;
+/* 
+  ${mq.md} {
+    grid-template-columns: 50% 50%;
+  } */
+`;
+
+
+const Logo = styled.div`
+position: relative;
+width: 35%;
+margin-top: 5.5rem;
+
+${mq.md}{
+
+  margin-top: 9.5rem;
+}
+
+`;
+
+
+const SectionTitle = styled.h1`
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+  text-align: center;
+
+  ${mq.md} {
+    text-align: left;
+  }
+`;
+
+
+const Copy = styled.p``;
+
